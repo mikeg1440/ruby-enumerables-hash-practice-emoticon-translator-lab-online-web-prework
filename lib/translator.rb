1288048ba1ebf2c047476f516a30c80805af3bfc
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-31 02:36:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,59 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  file_hash = YAML.load_file(file)
+  new_hash = {
+    :get_meaning => {},
+    :get_emoticon => {}
+  }
+  
+  file_hash.each do |key, array|
+    puts "Key: #{key}\nArray: #{array}"
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(eng_emoticon)
+  emoticons = load_library
+  emoticons_key = ""
+
+  emoticons.each do |key, array|
+    
+    array.select do |elem| 
+      if elem == eng_emoticon
+        emoticons_key = key
+      end
+      
+    end
+
+  end
+  
+  if emoticons_key == ""
+    return "Sorry, that emoticon was not found"
+  end
+  
+  emoticons[emoticons_key][1]
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(jap_emoticon)
+  emoticons = load_library
+  english_translation = ""
+  
+  emoticons.each do |key, array|
+    
+    array.select do |elem|
+      if elem == jap_emoticon
+        english_translation = key
+      end
+    end
+    
+  end
+  
+  if english_translation == "" 
+    return "Sorry, that emoticon was not found"
+  end
+  
+  english_translation
 end
+
+# get_english_meaning("☜(⌒▽⌒)☞")
+load_library("lib/emoticons.yml")
