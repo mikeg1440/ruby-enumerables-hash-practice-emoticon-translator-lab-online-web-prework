d4a4f3cf8b02d49899d7d047d41e9e64a2f19cd4
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-31 03:32:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -52,7 +52,7 @@ def get_japanese_emoticon(eng_emoticon)
   #   return "Sorry, that emoticon was not found"
   # end
   
-  p japanese_emoticon
+  japanese_emoticon
 end
 
 def get_english_meaning(jap_emoticon)
