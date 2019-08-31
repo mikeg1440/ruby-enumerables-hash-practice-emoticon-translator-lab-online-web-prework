84073e24718d125442b86b9ca3df64598be7f445
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-31 03:26:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(eng_emoticon)
   emoticons = emoticons[:get_emoticon]
   emoticons_key = ""
 
-  emoticons.each do |jap_emoticon, emoticon|
+  emoticons.each do |emoticon, jap_emoticon|
     
     #puts "K: #{jap_emoticon}\nA: #{emoticon}"
   
